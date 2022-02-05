e145e130703a06c90b725cf247436b82f6d267c7
Eduardo Pereira <augusto.pereira@protonmail.com>
2022-02-05 18:20:12 -0300

    feat: validation middleware

---
+++ src/middlewares/validation.middleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+export const validate =
+  (schema: any) => async (req: Request, res: Response, next: NextFunction) => {
+    const resource = req.body;
+    try {
+      await schema.validate(resource);
+    } catch (e) {
+      res.status(400).json({ error: e.errors.join(", ") });
+    }
+  };
