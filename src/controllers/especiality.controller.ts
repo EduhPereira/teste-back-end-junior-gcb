54ca1c6613c85536f9e9dd0486e34b525485dc72
Eduardo Pereira <augusto.pereira@protonmail.com>
2022-02-06 21:31:20 -0300

    feat: especiality controller

---
+++ src/controllers/especiality.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from "express";
+import { getRepository } from "typeorm";
+import Especiality from "../entities/Especiality";
+
+export const create = (req: Request, res: Response, next: NextFunction) => {};
+export const getAll = (req: Request, res: Response, next: NextFunction) => {};
+export const getOne = (req: Request, res: Response, next: NextFunction) => {};
+export const update = (req: Request, res: Response, next: NextFunction) => {};
+export const remove = (req: Request, res: Response, next: NextFunction) => {};
