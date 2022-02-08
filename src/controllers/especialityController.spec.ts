913a3946b350c5fca781ded8405be68e324c74ba
Eduardo Pereira <augusto.pereira@protonmail.com>
2022-02-08 16:30:34 -0300

    feat: especiality controller tests

--- src/controllers/especialityController.spec.ts
+++ src/controllers/especialityController.spec.ts
@@ -10,4 +10,47 @@ describe("Especiality Controller Test", () => {
     const connection = getConnection();
     await connection.close();
   });
+
+  let especialityId = "";
+  const especialityPayload = {
+    nome: "Cardiologia",
+  };
+
+  it("Should able to register a new especiality", async () => {
+    const response = await request(app)
+      .post("/especialities")
+      .send(especialityPayload);
+    especialityId = response.body.id;
+
+    expect(response.body).toHaveProperty("nome");
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.nome).toBe(especialityPayload.nome);
+    expect(response.status).toBe(201);
+  });
+  it("Should be able to get the especiality informations", async () => {
+    const response = await request(app).get(`/especialities/${especialityId}`);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("nome");
+    expect(response.status).toBe(200);
+  });
+  it("Should be able to update the created especiality", async () => {
+    const response = await request(app)
+      .put(`/especialities/${especialityId}`)
+      .send({
+        nome: "Alergologia",
+      });
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("nome");
+    expect(response.body.nome).toBe("Alergologia");
+    expect(response.status).toBe(200);
+  });
+  it("Should be able to delete the created especiality", async () => {
+    const response = await request(app).delete(
+      `/especialities/${especialityId}`
+    );
+
+    expect(response.status).toBe(204);
+  });
 });
